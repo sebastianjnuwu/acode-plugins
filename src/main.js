205c2f6a5719a2281441595201fef49da7aedea3
sebastianjnuwu <sebastianjnuwu@gmail.com>
2023-07-30 01:50:25 -0300

    v1.0.2 - (not publish)

--- src/main.js
+++ src/main.js
@@ -1,35 +1,62 @@
-// importing necessary files
 import plugin from '../plugin.json';
 import style from './styles/style.scss';
+const settings = acode.require('settings');
+const { editor } = editorManager;
 
-// main class
-class icon {
-	// function to start
+const theme = ['Night-Owl'];
+
+class Editor {
+	
 	async init() {
-		// eslint-disable-next-line no-undef
-		this.style = tag('style', { textContent: style });
-		// eslint-disable-next-line no-undef
-		document.head.append(this.style);
-	}
+		theme.forEach(name => {
+			ace.define(`ace/theme/${name}.css`, ['require', 'exports', 'module'], function (require, exports, module) {
+				module.exports = style;
+			});
+			ace.define(
+				`ace/theme/${name}`,
+				['require', 'exports', 'module', `ace/theme/${name}.css`, 'ace/lib/dom'],
+				function (require, exports, module) {
+					exports.isDark = true;
+					exports.cssClass = `ace-${name}`;
+					exports.cssText = require(`./${name}.css`);
+					const dom = require('../lib/dom');
+					dom.importCssString(exports.cssText, exports.cssClass, false);
+				},
+			);
+			window.require(['ace/theme/' + name], function (m) {
+				if (typeof module == 'object' && typeof exports == 'object' && module) {
+					module.exports = m;
+				}
+			});
 
-	// function to kill
+			ace.require('ace/ext/themelist').themes.push({
+				caption: name.split('-').map(x => x[0].toUpperCase() + x.slice(1)).join(' '),
+				theme: 'ace/theme/' + name,
+				isDark: true,
+			});
+			
+  const current = settings.get('editorTheme');
+	if (current === name) {
+		 editor.setTheme(`ace/theme/${name}`);
+	   settings.update({ editorTheme: name });
+	};
+		
+		});
+	};
+	
 	async destroy() {
-		// removing applied style
-		this.style.remove();
+		editor.setTheme('ace/theme/nord_dark');
+		settings.update({ editorTheme: 'nord_dark' });
 	}
 }
 
-// eslint-disable-next-line no-undef
 if (window.acode) {
-	// eslint-disable-next-line no-undef
+
 	acode.setPluginInit(plugin.id, async (url, page, { cacheFileUrl, cacheFile }) => {
 		if (!url.endsWith('/')) return (url += '/');
-
-		// settings
-		new icon().url = url;
-		new icon().init(page, cacheFile, cacheFileUrl);
+		new Editor().url = url;
+		new Editor().init(page, cacheFile, cacheFileUrl);
 	});
 
-	// eslint-disable-next-line no-undef
-	acode.setPluginUnmount(plugin.id, () => new icon().destroy());
-}
+	acode.setPluginUnmount(plugin.id, () => new Editor().destroy());
+};
